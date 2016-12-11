6b4ba4dd4e8dac772904a854ed91a6a850b371b1
gunjank <sngunjan@yahoo.com>
2016-12-10 22:50:05 -0500

    added more test cases

--- test/test.js
+++ test/test.js
@@ -5,9 +5,13 @@ var server = require("../src/index");
 
 lab.experiment("Basic HTTP Tests", function () {
     lab.before({
-        timeout: 10000
+        timeout: 2000
     }, (done) => {
         done();
+    });
+    lab.after((done) => {
+        server.stop(done);
+        console.log(" server stopped");
     });
     // tests
     lab.test("GET /v1/feeds (endpoint test)", function (done) {
@@ -17,10 +21,38 @@ lab.experiment("Basic HTTP Tests", function () {
         };
         // server.inject lets you simulate an http request
         server.inject(options, function (response) {
-            //console.log("Session: %s" + response)
             Code.expect(response.statusCode).to.equal(200); //  Expect http response status code to be 200 ("Ok")
-            Code.expect(response.result.data).to.have.length(5); // Expect result to be "Hello Timmy!" (12 chars long)
-            server.stop(done); // done() callback is required to end the test.
+            Code.expect(response.result.data).to.have.length(5); // Expect result.data has 5 records
+            done();
+        });
+    });
+    lab.test("GET /v1/feeds/{feedId} (endpoint test)", function (done) {
+        var options = {
+            method: "GET",
+            url: "/v1/feeds/1234"
+        };
+        // server.inject lets you simulate an http request
+        server.inject(options, function (response) {
+            Code.expect(response.statusCode).to.equal(200); //  Expect http response status code to be 200 ("Ok")
+            //Code.expect(response.result.data).to.have.length(5); // Expect result.data has 5 records
+            done();
+        });
+    });
+    lab.test("POST /v1/feeds/{feedId} (endpoint test)", function (done) {
+        var options = {
+            method: "POST",
+            url: "/v1/feed",
+            payload: {
+                feedId: "1234",
+                feedDesc: "this is feed desc"
+            }
+        };
+        // server.inject lets you simulate an http request
+        server.inject(options, function (response) {
+            Code.expect(response.statusCode).to.equal(200); //  Expect http response status code to be 200 ("Ok")
+            Code.expect(response.result.feedId).to.equal("1234"); // Expect result feedId as passed in payload
+            Code.expect(response.result.feedDesc).to.equal("this is feed desc"); // Expect result feedDesc as passed in payload
+            done();
         });
     });
 });
